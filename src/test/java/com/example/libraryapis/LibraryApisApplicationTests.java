c3f79662d45a32b7d5d6eba0700308329bbd64d2
rsr986 <rsr986@gmail.com>
2024-02-28 14:36:38 +0530

    Initial commit

---
+++ src/test/java/com/example/libraryapis/LibraryApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libraryapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
